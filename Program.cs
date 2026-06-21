964074bfd364870dad94941ef1c15c7113ec42b0
bonamigo <lucashbo321@gmail.com>
2026-06-20 23:53:28 -0300

    changed Swagger description

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "TechFix API",
+        Description = "Core API for the TechFix Management System. Built with .NET 8.",
+        Contact = new OpenApiContact
+        {
+            Name = "Lucas Henrique Bonamigo Oliveira",
+            Url = new Uri("https://www.linkedin.com/in/luckkkkas")
+        }
+    });
+});
 
 var app = builder.Build();
 
